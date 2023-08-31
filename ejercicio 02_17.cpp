f2a3d1ddb034cc9cc5b39fc47641bc1f95f1a4c9
YersonChui <142272272+YersonChui@users.noreply.github.com>
2023-08-30 23:31:16 -0400

    Update ejercicio 02_17.cpp

--- ejercicio 02_17.cpp
+++ ejercicio 02_17.cpp
@@ -1,7 +1,7 @@
-// Fecha creación: 29/08/2023
-// Fecha modificación: 30/08/2023
-// Número de ejericio: 17
-// Problema planteado: Se quiere realizar un programa que lea por teclado las 5 notas obtenidas por un alumno (comprendidas entre 0 y 100). A continuación, debe mostrar todas las notas, la nota media, la nota más alta que ha sacado y la menor.
+// Fecha creaciÃ³n: 29/08/2023
+// Fecha modificaciÃ³n: 30/08/2023
+// NÃºmero de ejericio: 17
+// Problema planteado: Se quiere realizar un programa que lea por teclado las 5 notas obtenidas por un alumno (comprendidas entre 0 y 100). A continuaciÃ³n, debe mostrar todas las notas, la nota media, la nota mÃ¡s alta que ha sacado y la menor.
 
 #include <iostream>
 using namespace std;
@@ -24,14 +24,14 @@ int main() {
                     minimo = notas[i]; //nota min
             }
     }
-    double media = suma / 5;
+    double media = suma/5;
     cout << "Las notas del alumno son: " << endl;
     for (int i=0; i<5; i++) {
             cout << notas[i] << " ";
     }cout << endl;
-    cout << "La nota media es " << media << endl;
-    cout << "La nota más alta es " << maximo << endl;
-    cout << "La nota más baja es " << minimo << endl;
+    cout << "La nota media es "<< media << endl;
+    cout << "La nota mas alta es "<<maximo <<endl;
+    cout << "La nota mas baja es "<<minimo<<endl;
 
     return 0;
 }
